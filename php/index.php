623c4ed36ff9ce4dc7a9fe83af48d85567c7a44c
realcreativeprogrammer <realcreativeprogrammer@gmail.com>
2022-06-11 16:48:56 +0300

    first

---
+++ php/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?php echo 'my title'; ?>
+    </title>
+</head>
+<body>
+  <h1>
+  <?php echo 'my body'; ?>
+
+  </h1> 
+
+</body>
+</html>
